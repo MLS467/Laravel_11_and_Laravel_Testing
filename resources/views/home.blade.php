de9c1009d3c719e39038afe3b97928a35b30d8ba
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-14 15:55:29 -0300

    Laravel 11 tutorial #13 Components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,9 @@
 
     <h2>{{ rand(1,100) }}</h2>
 
-
+    <x-message-banner msg="Página de teste" class="success" />
+    <x-message-banner msg="Página de teste" class="error" />
+    <x-message-banner msg="Página de teste" class="warning" />
+    <x-message-banner msg="Página de teste" class="info" />
     <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
 </div>
