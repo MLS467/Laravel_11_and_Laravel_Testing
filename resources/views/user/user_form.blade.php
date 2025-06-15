2bc14ed0151a467ed1cd592eebdb9127ec706de2
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-14 22:29:42 -0300

    Laravel 11 tutorial #15 Form handling with checkbox, radio button and dropdown

--- resources/views/user/user_form.blade.php
+++ resources/views/user/user_form.blade.php
@@ -21,10 +21,57 @@
             <input type="password" class="form-control" id="password" name="password"
                 placeholder="Leave blank to keep current password">
         </div>
+
+        <div>
+            <h4>Habilidades do usuário.</h4>
+            <div>
+                <input type="checkbox" name="php" value="1" id="php">
+                <label for="php">PHP</label>
+            </div>
+            <div>
+                <input type="checkbox" name="js" value="1" id="javascript">
+                <label for="javascript">Javascript</label>
+            </div>
+            <br><br>
+        </div>
+
+        <div>
+            <h4>Gênero do usuário.</h4>
+            <div>
+                <input type="radio" name="genero" value="feminino" id="feminino" checked>
+                <label for="feminino">Feminino</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" name="genero" value="masculino" id="masculino">
+                <label for="masculino">Masculino</label>
+            </div>
+            <br><br>
+        </div>
+
+        <div>
+            <h4>Idade do usuário</h4>
+            <input type="range" min="0" max="100" value="50" class="form-range" id="customRange1" name="idade">
+            <label for="customRange1">Valor do range: <span id="rangeValue">50</span></label>
+        </div>
+
         <div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
 
     </form>
+
+    <script>
+    const rangeInput = document.getElementById('customRange1');
+    const rangeValue = document.getElementById('rangeValue');
+
+    // Atualiza ao carregar a página
+    rangeValue.textContent = rangeInput.value;
+
+    // Atualiza em tempo real ao mover o controle
+    rangeInput.addEventListener('input', () => {
+        rangeValue.textContent = rangeInput.value;
+    });
+    </script>
 </div>
 @endsection
