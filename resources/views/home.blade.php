8d5d460411f0e9e908c1df80187a65a966db1e1b
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-15 22:37:45 -0300

    Laravel 11 tutorial #18 Custom validation rule

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends("common.common")
 @section("content")
 <div>
-
+    <h3>{{ URL::current() }}</h3>
+    <h3>{{ url()->full() }}</h3>
     @foreach ($valores as $val )
     <h1>{{ $val }}</h1>
     @endforeach
@@ -10,5 +11,6 @@
     <!-- <x-message-banner msg="Página de teste" class="success" /> 
     
     <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+    <a href={{ url()->to('about',['batata']) }}>about --> treta</a>
 </div>
 @endsection
