c69503160dc900a5b705ceb906512a5a31c17dcc
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-16 12:43:43 -0300

    Laravel 11 tutorial #21 Route Group with Prefix

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
+use App\Http\Controllers\Home21Controller;
 use Illuminate\Support\Facades\Route;
 
+
+// Controller
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return  "<h1>Hello, World</h1>";
-});
+Route::prefix('/student')
+    ->group(function () {
+        Route::controller(Home21Controller::class)
+            ->group(function () {
+
+                Route::get("/", "index")->name('hm');
+            });
+    });
+
+Route::prefix('/student/Brazil')
+    ->group(function () {
+        Route::controller(Home21Controller::class)
+            ->group(function () {
+
+                Route::get("/", "index");
+            });
+    });
