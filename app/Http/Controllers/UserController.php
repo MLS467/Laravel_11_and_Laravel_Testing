f3da5c2e57c4daa80c578bf5b4afc2c91bc176dd
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-18 13:36:11 -0300

    Laravel 11 tutorial #30 Http Client

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class UserController
 {
@@ -14,5 +15,13 @@ class UserController
         if (!$users) return [];
 
         return view('welcome', ['content_users' => $users]);
+    }
+
+    public function getUser()
+    {
+
+        $user_placeholder = Http::get("https://jsonplaceholder.typicode.com/users");
+
+        return view('users', ['content_users' => json_decode($user_placeholder)]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\studentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', UserController::class)->name('users.index');
 
 Route::get('/student', [studentController::class, 'getStudents'])
     ->name('student');
+
+
+Route::prefix('/user')->group(function () {
+    Route::get('/', UserController::class)->name('users');
+    Route::get('/api', [UserController::class, 'getUser'])->name('getUser');
+});
